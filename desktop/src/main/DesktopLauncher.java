fb79056bfa4fc9e2ab122ca3893f62814c172c7f
Serhii Hryhorenko <hryhorenkoser@gmail.com>
2022-05-24 17:08:13 +0300

    initial

---
+++ desktop/src/main/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package main;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(30);
+		config.setTitle("Doodle Jump");
+		new Lwjgl3Application(new Game(), config);
+	}
+}
